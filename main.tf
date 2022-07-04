75165980381eef781e64b137c5c2280211ffd84c
mclsylva <88355017+mclsylva@users.noreply.github.com>
2022-07-04 17:18:16 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,66 +1,34 @@
-#Configurar o operador
 provider "aws" {
-    region = "us-east-1"
-  
+    region = "us-east-2"
+
+    ## Forma 1
+    #access_key = ""
+    #secret_key = ""
+
+    ## Forma 2
+    # export AWS_SECRET_ACCESS_KEY=
+    # export AWS_ACCESS_KEY_ID=
+
+    ## Forma 3
+    # Instalar o AWS CLI
+    # AWS configure
 }
 
-#Criar o nosso bucket
-resource "aws_s3_bucket" "s3_terraform" {
-    bucket = "ortiztfteste"
-    force_destroy = "true"
+resource "aws_instance" "efs_server_2" {
+    ami = var.ami_id # Define a imagem do SO
+    instance_type = var.instance_type # Define qual é o tipo da instância
+    availability_zone = var.avail_zone # Defino qual é a minha zona de disponibilidade
+    key_name = "ohio"
+
+    user_data = <<EOF
+                    #!/bin/bash
+                    yum install -y amazon-efs-utils
+                    mkdir /mnt/efs
+                    mount -t efs -o tls fs-0def155c622f750fc:/ /mnt/efs
+                EOF
+
     tags = {
-        Name = "Test Terraform"
-        Env = "Test Env"
+        Name = "${var.env_prefix}-server"
     }
-  
-}
 
-#Setar permissão
-resource "aws_s3_bucket_acl" "s3_terraform_acl" {
-    bucket = aws_s3_bucket.s3_terraform.id
-    acl = "public-read"
-  
-}
-
-#Habilitar o versionamento do bucket
-resource "aws_s3_bucket_versioning" "se_terraform_version" {
-    bucket = aws_s3_bucket.s3_terraform.id
-    versioning_configuration {
-      status = "Enabled"
-    }
-  
-}
-
-resource "aws_s3_bucket_lifecycle_configuration" "s3_terraform-lifecycle" {
-    bucket = aws_s3_bucket.s3_terraform.id
-
-    rule {
-        id = "lifecycle_30_60"
-
-        noncurrent_version_transition {
-          noncurrent_days = 30
-          storage_class = "STANDARD_IA"
-        }
-
-        noncurrent_version_transition {
-          noncurrent_days = 60
-          storage_class = "GLACIER"
-        }
-
-        status = "Enabled"
-
-    }
-  
-}
-
-#Habilitar a criptografia
-resource "aws_s3_bucket_server_side_encryption_configuration" "s3_terraform_cript" {
-    bucket = aws_s3_bucket.s3_terraform.id
-
-    rule {
-        apply_server_side_encryption_by_default {
-          sse_algorithm = "AES256"
-        }
-    }
-  
-}
+} 
